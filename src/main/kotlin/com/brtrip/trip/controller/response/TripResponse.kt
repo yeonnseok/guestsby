c2b66624a6f1cfff83f50c23530135ec00f47fb6
mingukang <mgkang0317@gmail.com>
2021-08-28 22:50:50 +0900

    add: tripReponse에 tripId 추가

--- src/main/kotlin/com/brtrip/trip/controller/response/TripResponse.kt
+++ src/main/kotlin/com/brtrip/trip/controller/response/TripResponse.kt
@@ -5,6 +5,7 @@ import com.brtrip.path.controller.response.PathResponse
 import com.brtrip.trip.domain.Trip
 
 data class TripResponse(
+    val id: Long,
     val title: String,
     val startDate: String,
     val endDate: String,
@@ -16,6 +17,7 @@ data class TripResponse(
             val paths = trip.tripPaths.map { it.path }
             val places = paths.map { path -> path.pathPlaces.map { it.place } }
             return TripResponse(
+                id = trip.id!!,
                 title = trip.title,
                 startDate = trip.startDate.format_yyyy_MM_dd(),
                 endDate = trip.endDate.format_yyyy_MM_dd(),
