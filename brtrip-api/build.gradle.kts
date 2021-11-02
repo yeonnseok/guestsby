6ff8902230ff6d5aa6bfb76dfc7ded1b5b27954d
mingukang <mgkang0317@gmail.com>
2021-11-02 15:47:29 +0900

    멀티 모듈 의존성 설정

--- brtrip-api/build.gradle.kts
+++ brtrip-api/build.gradle.kts
@@ -24,6 +24,7 @@ configurations {
 }
 
 dependencies {
+    implementation(project(":brtrip-common"))
     implementation("io.jsonwebtoken:jjwt:0.9.1")
     implementation("com.google.code.gson:gson:2.8.6")
     implementation("com.google.guava:guava:30.1.1-jre")
--- brtrip-common/build.gradle.kts
+++ brtrip-common/build.gradle.kts
@@ -1,7 +1,26 @@
+import org.springframework.boot.gradle.tasks.bundling.BootJar
+
+tasks.getByName<BootJar>("bootJar") {
+    enabled = false
+}
+
+tasks.getByName<Jar>("jar") {
+    enabled = true
+}
+
 plugins {
+    id("org.springframework.boot")
+    id("io.spring.dependency-management")
+    id("org.asciidoctor.convert")
     kotlin("jvm")
+    kotlin("plugin.spring")
+    kotlin("plugin.jpa")
 }
 
 dependencies {
-    implementation(kotlin("stdlib"))
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-security")
 }
