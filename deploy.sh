c81cf8a522ae5b406d0492f776ba36a2ee0674bd
mingukang <mgkang0317@gmail.com>
2021-11-14 00:51:10 +0900

    docker 파일 및 deploy 로그 수정

--- deploy.sh
+++ deploy.sh
@@ -4,12 +4,6 @@ REPOSITORY=/home/ubuntu/server
 cd $REPOSITORY
 
 APP_NAME=brtrip
-JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep '.jar' | tail -n 1)
-JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
-JVM_OPTS="
--Dspring.profiles.active=live
--Dspring.config.location=$REPOSITORY/application-oauth.yml,$REPOSITORY/application.yml
-"
 
 CURRENT_PID=$(pgrep -f $APP_NAME)
 
@@ -17,12 +11,25 @@ if [ -z "$CURRENT_PID" ]
 then
   echo "> 종료할 것 없음." >> /home/ubuntu/deploy.log
 else
-  echo "> sudo kill -15 $CURRENT_PID" >> /home/ubuntu/deploy.log
-  kill -15 "$CURRENT_PID" >> /home/ubuntu/deploy.log 2>&1
-  sleep 5
+
+if [ -d $REPOSITORY/build ]; then
+  rm -rf $REPOSITORY/build
+  mkdir -vp $REPOSITORY/build
+fi
+
+docker stop $APP_NAME
+sleep 5
+docker rm $APP_NAME
+sleep 5
+
+if [[ "$(docker images -q mingukang-kr/$APP_NAME:latest 2> /dev/null)" != "" ]]; then
+  docker rmi -f $(docker images -q)
+fi
+
 fi
 
 echo "> $JAR_PATH 배포"
-nohup java $JVM_OPTS -jar "$JAR_PATH" >> /home/ubuntu/deploy.log 2>&1 &
+docker pull $APP_NAME
+docker run --env-file /home/ubuntu/.env --publish 8080:8080 -it --detach --name $APP_NAME /bin/bash
 
 echo "[$(date)] server deploy" >> /home/ubuntu/deploy.log
